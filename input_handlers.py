5c404280478c4dfd3cb0cd0443139232adb2c60a
Lindsey Brooks <elbrookso@gmail.com>
2019-10-04 11:40:35 -0600

    Pathfinding, Combat, Death

--- input_handlers.py
+++ input_handlers.py
@@ -2,15 +2,24 @@ import tcod as libtcod
 
 def handle_keys(key):
     # Movement keys
-    if key.vk == libtcod.KEY_UP:
+    if key.vk == libtcod.KEY_UP or key.vk == libtcod.KEY_KP8:
         return {'move': (0, -1)}
-    elif key.vk == libtcod.KEY_DOWN:
+    elif key.vk == libtcod.KEY_DOWN or key.vk == libtcod.KEY_KP2:
         return {'move': (0, 1)}
-    elif key.vk == libtcod.KEY_LEFT:
+    elif key.vk == libtcod.KEY_LEFT or key.vk == libtcod.KEY_KP4:
         return {'move': (-1, 0)}
-    elif key.vk == libtcod.KEY_RIGHT:
+    elif key.vk == libtcod.KEY_RIGHT or key.vk == libtcod.KEY_KP6:
         return {'move': (1, 0)}
-
+    elif key.vk == libtcod.KEY_KP1:
+        return {'move': (-1, 1)}
+    elif key.vk == libtcod.KEY_KP3:
+        return {'move': (1, 1)}
+    elif key.vk == libtcod.KEY_KP7:
+        return {'move': (-1, -1)}
+    elif key.vk == libtcod.KEY_KP9:
+        return {'move': (1, -1)}
+        
+                        
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         # Alt+Enter: toggle full screen
         return {'fullscreen': True}
--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -3,6 +3,9 @@ from random import randint
 from map_objects.tile import Tile
 from map_objects.rectangle import Rect
 from entity import Entity
+from components.ai import BasicMonster
+from components.fighter import Fighter
+from render_functions import RenderOrder
 
 class GameMap:
     def __init__(self, width, height):
@@ -97,9 +100,15 @@ class GameMap:
 
             if not any([entity for entity in entities if entity.x == x and entity.y == y]):
                 if randint(0, 100) < 80:
-                    monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True)
+                    fighter_component = Fighter(hp=10, defense=0, power=3)
+                    ai_component = BasicMonster()
+
+                    monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component)
                 else:
-                    monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks=True)
+                    fighter_component = Fighter(hp=16, defense=1, power=4)
+                    ai_component = BasicMonster()
+
+                    monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks=True, render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component)
 
                 entities.append(monster)
 
