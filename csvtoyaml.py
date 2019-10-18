33e192e56f02c790fef898c397acc148079f8aec
Brooks (Workstation Admin) <lbbrooks.adm@sts.ad.ualberta.ca>
2019-10-17 21:22:31 -0600

    Testing

--- csvtoyaml.py
+++ csvtoyaml.py
@@ -6,18 +6,30 @@ from time import sleep
 
 
 
-yaml_stream = open(os.path.join('test.yaml'), 'rw')
+yaml_stream = open(os.path.join('test.yaml'), 'r', newline="")
 yaml_object = yaml.load(yaml_stream)
+yaml_store = {}
 
 
-with open('test.csv', mode='r', newline='') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            line_count += 1
-        else:
-            yaml_object[row[0]]['name'] = row[1]
+with open('test.csv', mode='r') as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter=',')
+	line_count = 0
+	first_row = []
+	for row in csv_reader:
+		x=0
+		if line_count == 0:
+			line_count += 1
+			first_row = row
+
+		else:
+			x = 1
+			for i in row:
+				yaml_store = {row[0]: {first_row[x]: row[x]}}
+				x += 1
+				print(yaml_store)
+				# yaml_stream[row[0]] = {}
+				# yaml_store[row[0]][first_row[x]] = row[x]
+				
 
 
 # for i in yaml_file[0]:
@@ -31,5 +43,5 @@ with open('test.csv', mode='r', newline='') as csv_file:
 #             row.append(yaml_file[i][x])
 #         csv_writer.writerow(row)
 #         row = []
-            
+			
 
