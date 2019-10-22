c3a6776c8df7bdd89d779a398f8f04e6cb160fab
Brooks (Workstation Admin) <lbbrooks.adm@sts.ad.ualberta.ca>
2019-10-22 15:56:56 -0600

    test

--- components/animator.py
+++ components/animator.py
@@ -5,6 +5,8 @@ class Animator:
     anim_type = None
     delta_x = None
     delta_y = None
+    x = None
+    y = None
     time = None
     
     def update(self):
@@ -14,12 +16,21 @@ class Animator:
             if self.time <= 0:
                 self.anim_type = None
                 self.time = None
+                self.x = None
+                self.y = None
 
             if self.anim_type == 'move':
+
+                if self.x == None and self.y == None:
+                    self.x = self.owner.x
+                    self.y = self.owner.y
+                    
                 final_delta_x = self.delta_x * (libtcod.sys_get_last_frame_length() / self.time)
                 final_delta_y = self.delta_y * (libtcod.sys_get_last_frame_length() / self.time)
-                self.owner.x += final_delta_x
-                self.owner.y += final_delta_y
+                self.x += final_delta_x
+                self.y += final_delta_y
+                self.owner.x += round(self.x)
+                self.owner.y += round(self.y)
                 self.delta_x -= final_delta_x
                 self.delta_y -= final_delta_y
                 self.time -= libtcod.sys_get_last_frame_length()
