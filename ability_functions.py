9b90ec48cbaa9bd61ca2f82b72292d6cef766a73
Brooks (Workstation Admin) <lbbrooks.adm@sts.ad.ualberta.ca>
2019-10-22 15:57:00 -0600

    test

--- ability_functions.py
+++ ability_functions.py
@@ -6,8 +6,14 @@ def tackle(*args, **kwargs):
     caster = args[0]
     target_x = kwargs.get('target_x')
     target_y = kwargs.get('target_y')
+    entities = kwargs.get('entities')
     caster.animator.move(target_x, target_y, 30)
     results = []
-    results.append({'message': Message('I eat some poop', libtcod.green)})
+    for entity in entities:
+        if entity.x == target_x and entity.y == target_y and hasattr(entity, 'fighter'):
+            results.append({'message': Message('{0} charges towards the {1}!'.format(caster.name, entity.name), libtcod.green)})
+            return results
+    else:    
+        results.append({'message': Message('{0} charges forward!'.format(caster.name), libtcod.green)})
 
     return results
